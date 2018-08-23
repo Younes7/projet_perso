4b0d98800a5a1ec1341d01b70a3a221c78d34e97
Younes7 <younus.sayah@gmail.com>
2018-08-23 14:22:49 +0200

    model users + route enregistrement + bcrypt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 // GET /api/users/test
 router.get('/test', (req, res) => res.json({ users: 'younes' }));
+
+// GET /api/users/register
+router.post('/register', (req,res) => {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(user) {
+        return res.status(400).json({ email: 'Email déjà existant' });
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s:'200',
+          r:'pg',
+          d:'mm'
+        });
+
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err))
+          });
+        });
+      }
+    });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const users = require('./routes/api/users')
+const users = require('./routes/api/users');
 
 const app = express();
 
+//BodyParser
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.json());
+
 // Config base de données
 const db = require('./config/keys').mongoURI;
+
 mongoose
   .connect(db)
   .then(() => console.log('Connecté à mongo'))
